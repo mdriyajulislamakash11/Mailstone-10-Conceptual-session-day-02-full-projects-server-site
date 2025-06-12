a9f8d0db2922f9269a04f2be90e7199691823868
Akash <hafejmohammadakash@gmail.com>
2025-06-12 11:50:10 +0600

    post methods test korlam post man diye

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Midleware
-app.use(cors());            //cors()    বাইরের ডোমেইন থেকে API রিকোয়েস্টের অনুমতি দেয়
-app.use(express.json());    //express.json()    JSON ডেটাকে বুঝে ফেলে এবং JavaScript object-এ রূপান্তর করে
+app.use(cors()); //cors()    বাইরের ডোমেইন থেকে API রিকোয়েস্টের অনুমতি দেয়
+app.use(express.json()); //express.json()    JSON ডেটাকে বুঝে ফেলে এবং JavaScript object-এ রূপান্তর করে
 
 //Mongo DB conneted code:
 
 // M-10-Con-Sess-day-2
-// ZxHzRvg9KvlAruqL
+// ZxHzRvg9KvlAruqL 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://M-10-Con-Sess-day-2:ZxHzRvg9KvlAruqL@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri =
+  "mongodb+srv://M-10-Con-Sess-day-2:ZxHzRvg9KvlAruqL@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,16 +23,31 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+    app.post("/data", (req, res) =>{
+        const data = req.body;
+        res.json({
+            status: true,
+            data : data,
+        })
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,11 +55,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port: ${port}`);
 });
