5c65befa3a421c113c1825372e3dcfa206f1b776
Akash <hafejmohammadakash@gmail.com>
2025-06-12 10:49:51 +0600

    idleWare er kaj ki seta bollam

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
 //Midleware
-app.use(cors())
-app.use(express())
+app.use(cors());            //cors()    বাইরের ডোমেইন থেকে API রিকোয়েস্টের অনুমতি দেয়
+app.use(express.json());    //express.json()    JSON ডেটাকে বুঝে ফেলে এবং JavaScript object-এ রূপান্তর করে
 
-//Mongo DB conneted code: 
-
-
+//Mongo DB conneted code:
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
