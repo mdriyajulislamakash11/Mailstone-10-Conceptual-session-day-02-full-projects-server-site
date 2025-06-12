fcc4bf6a5e8dbadeb498710a91a5d99d1a1d0a86
Akash <hafejmohammadakash@gmail.com>
2025-06-12 10:41:21 +0600

    idleWare st korlam

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const cors = require('cors');
-const port = process.env.PORT || 5000
 const app = express()
+const port = process.env.PORT || 5000
 
+
+//Midleware
 app.use(cors())
 app.use(express())
+
+//Mongo DB conneted code: 
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
