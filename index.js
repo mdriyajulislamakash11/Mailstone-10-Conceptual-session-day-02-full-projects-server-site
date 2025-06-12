0f0374c8ab88d7041bea790214e9b89dd4a7253d
Akash <hafejmohammadakash@gmail.com>
2025-06-12 10:39:12 +0600

    port modifi korlam

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const cors = require('cors');
+const port = process.env.PORT || 5000
 const app = express()
-const port = 3000
+
+app.use(cors())
+app.use(express())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
